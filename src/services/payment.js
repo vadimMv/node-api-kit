4637c7cf1ef84dc32582267e1987c65013fdac9f
Vadim M <vadimmukovozov@gmail.com>
2020-11-23 15:25:12 +0200

    create credit card  token

--- src/services/payment.js
+++ src/services/payment.js
@@ -1,6 +1,6 @@
 const request = require('../../lib/http');
-const { stripe_host, stripe_api_key, stripe_path, https_port } = require('../../config');
-module.exports = async (data) => {
+const { stripe_host, stripe_api_key, stripe_path, https_port , stripe_create_token} = require('../../config');
+const payment = async (data) => {
     const options = {
         hostname: stripe_host,
         port: https_port,
@@ -20,7 +20,34 @@ module.exports = async (data) => {
             email: obj.receipt_email,
             price: obj.amount,
         };
-    } catch (error) { 
+    } catch (error) {
         throw new Error("Cant complete payment");
     }
 };
+const payTokenCreate = async (data) => {
+    const options = {
+        hostname: stripe_host,
+        port: https_port,
+        path: stripe_create_token,
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${stripe_api_key}`,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data
+    };
+     console.log(options)
+    try {
+        const response = await request(options);
+        const obj = JSON.parse(response);
+        console.log(response)
+        return obj;
+    } catch (error) {
+        throw new Error("Cant create card token");
+    }
+};
+
+module.exports = {
+    payment,
+    payTokenCreate
+}
